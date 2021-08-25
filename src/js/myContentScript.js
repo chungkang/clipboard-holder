c0a11cb0e8fc82313032e591fce18fefa6ad8154
chungkang <chungkang.c@gmail.com>
2021-08-26 00:15:27 +0900

    'add_dragged_text'

--- src/js/myContentScript.js
+++ src/js/myContentScript.js
@@ -1,10 +1,9 @@
+const selectedText = window.getSelection();
 
 window.onkeydown = (e) => {
- if(e.ctrlKey && e.which==67){
-    alert('복사');
+// ctrl+C를 눌렀을 경우에 선택한 텍스트가 있는 경우
+// e.keycode IE 지원을 안함->e.which 사용
+ if(e.ctrlKey && e.which==67 && selectedText!=''){
+    alert(selectedText);
  }
 }
-
-// e.keycode IE 지원을 안함
-// ctrl 17
-// c 67
